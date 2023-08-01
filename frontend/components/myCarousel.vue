0c446cad16b820dd76144e16f2d110a7a2a6ccb4
mac <2490572554@qq.com>
2023-08-02 03:48:00 +0800

    add：新增myCarousel组件

---
+++ frontend/components/myCarousel.vue
@@ -0,0 +1,33 @@
+<template>
+	<view>
+		<Carousel v-model="value" loop autoplay dots="none">
+			<CarouselItem>
+				<div class="demo-carousel">1</div>
+			</CarouselItem>
+			<CarouselItem>
+				<div class="demo-carousel">2</div>
+			</CarouselItem>
+			<CarouselItem>
+				<div class="demo-carousel">3</div>
+			</CarouselItem>
+			<CarouselItem>
+				<div class="demo-carousel">4</div>
+			</CarouselItem>
+		</Carousel>
+	</view>
+</template>
+
+<script setup name="MyCarousel">
+	import {
+		ref
+	} from 'vue'
+	const value = ref(0)
+</script>
+
+<style lang="scss">
+	.demo-carousel {
+		width: 100%;
+		height: 25vh;
+		background-color: #ccc;
+	}
+</style>
