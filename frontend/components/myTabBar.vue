08ea014bc907245fb5b972e1007151909d6e0fe6
mac <2490572554@qq.com>
2023-08-02 03:47:45 +0800

    add：新增myTabBar组件

---
+++ frontend/components/myTabBar.vue
@@ -0,0 +1,23 @@
+<template>
+	<view class="my-tabbar">
+		我是 tab
+	</view>
+</template>
+
+<script setup name="MyTabBar">
+
+</script>
+
+<style lang="scss">
+	.my-tabbar {
+		background-color: #333;
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		width: 100%;
+		height: 3.125rem; // 导航栏高度
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+	}
+</style>
